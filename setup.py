c1f1d4f8ff06c8db7f82a3e83b3e5d290e1c3fd1
Joe Nahmias <joe@nahmias.net>
2019-01-24 16:14:46 +0000

    convert to setuptools from distutils

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
         name="radicale_auth_PAM",
@@ -8,13 +8,17 @@ setup(
         description="PAM authentication plugin for Radicale",
         author="Joseph Nahmias",
         author_email="joe@nahmias.net",
+        url="https://gitlab.com/jello/radicale_auth_PAM",
+        install_requires=["pam",],
+        packages=find_packages(),
+        include_package_data=True,
+        license="GPL3+",
         classifiers=[
             "Development Status :: 4 - Beta",
             "Intended Audience :: System Administrators",
             "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+            "Operating System :: POSIX :: Linux",
             "Programming Language :: Python :: 3",
             "Topic :: System :: Systems Administration :: Authentication/Directory",
             ],
-        packages=["radicale_auth_PAM"],
-        install_requires=["pam",],
 )
